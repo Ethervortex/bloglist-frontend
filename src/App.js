73ecf1b05d47b1b44c421127f7b0f16510505417
Teemu Ruokokoski <truoko@gmail.com>
2023-05-02 12:17:51 +0300

    Tehtävät 5.2 - 5.4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import CreateForm from './components/CreateForm'
+import Notif from './components/Notif'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+  const [info, setInfo] = useState({ message: null })
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -16,60 +22,109 @@ const App = () => {
     )
   }, [])
 
+  // Tehtävä 5.2
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const notify = (message, type='info') => {
+    setInfo({ message, type })
+    setTimeout(() => {
+      setInfo({ message: null } )
+    }, 5000)
+  }
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user)) // Tehtävä 5.2
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+      notify('wrong username or password', 'error')
+      setUsername('')
+      setPassword('')
     }
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  )
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogUser')
+    setUser(null)
+  }
+
+  const handleTitle = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleAuthor = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleUrl = (event) => {
+    setNewUrl(event.target.value)
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+    blogService.create(blogObject).then((returnedBlog) => {
+      setBlogs(blogs.concat(returnedBlog))
+      notify(`A new blog ${newTitle} by ${newAuthor} added`)
+      setNewTitle('')
+      setNewAuthor('')
+      setNewUrl('')
+    })
+      .catch((error) => {
+        console.error('Error creating blog:', error)
+        notify('Error creating blog', 'error')
+      })
+  }
 
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
-        {loginForm()}
+        <Notif info={info} />
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+        />
       </div>
     )
   }
   return (
     <div>
-      <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <h2>Blogs</h2>
+      <Notif info={info} />
+      <p>{user.name} logged in<button onClick={handleLogout}>logout</button></p>
+      <b></b>
+      <h2>Create new</h2>
+      <CreateForm
+        addBlog={addBlog}
+        newTitle={newTitle}
+        handleTitle={handleTitle}
+        newAuthor={newAuthor}
+        handleAuthor={handleAuthor}
+        newUrl={newUrl}
+        handleUrl={handleUrl}
+      />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
